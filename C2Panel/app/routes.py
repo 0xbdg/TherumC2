ee49d24c66d46427da99f7513102193300eaf66e
?0 <benjamindguntara@protonmail.com>
2024-04-02 11:53:48 +0700

    lklkds;lfks

--- C2Panel/app/routes.py
+++ C2Panel/app/routes.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import render_template, request
+from app import app
 
-app = Flask(__name__)
-
-app.route("/")
+@app.route('/')
 def login():
-    return render_template("auth/login.html")
+    return render_template("login.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
